75c412fce96c20babfcb28436607139cb7aa558a
Exprezzzor <132411298+Exprezzzo@users.noreply.github.com>
2025-07-09 18:33:24 -0700

    Delete apps/lvgt-pwa-backup directory

--- apps/lvgt-pwa-backup/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-// File: apps/lvgt-pwa/pages/index.tsx
-import LVGTScreenHomeFinal from '../components/LVGT/LVGTScreenHomeFinal';
-
-export default function Home() {
-  return <LVGTScreenHomeFinal />;
-}
